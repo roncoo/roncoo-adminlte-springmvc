b910384fee28d2d3d39ca73d28b70cdefd115595
wujing <297115770@qq.com>
2016-11-07 10:23:36 +0800

    修改缓存类

--- src/main/java/com/roncoo/adminlte/service/impl/dao/impl/base/imple/CachedDaoImpl.java
+++ src/main/java/com/roncoo/adminlte/service/impl/dao/impl/base/imple/CachedDaoImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015-2016 RonCoo(http://www.roncoo.com) Group.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.roncoo.adminlte.service.impl.dao.impl.base.imple;
 
 import java.util.ArrayList;
@@ -24,7 +39,6 @@ import com.roncoo.adminlte.service.impl.dao.impl.base.CachedDao;
  * @param <K>
  * @param <V>
  */
-
 public abstract class CachedDaoImpl<K, V> implements CachedDao<K, V> {
 	private ConcurrentMap<K, V> cached = new ConcurrentHashMap<K, V>();
 
